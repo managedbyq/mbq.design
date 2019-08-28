1416c03b0b1013dd994fbfe52338296a141ed7f9
Matt Briancon <mattbriancon@users.noreply.github.com>
2019-08-28 11:16:25 -0400

    Fix gulp?

--- gulpfile.js
+++ gulpfile.js
@@ -15,19 +15,17 @@ gulp.task('concat-css', function(){
 });
 
 //  Minify stylesheets
-gulp.task('minify-css', function(done){
-  gulp.src('build/css/site.css')
+gulp.task('minify-css', function(){
+  return gulp.src('build/css/site.css')
     .pipe(cleancss())
     .pipe(gulp.dest('build/css/'));
-  done();
 });
 
 // Gzip stylesheets
-gulp.task('compress-css', function(done){
-  gulp.src('build/css/site.css')
+gulp.task('compress-css', function(){
+  return gulp.src('build/css/site.css')
   	.pipe(gzip())
   	.pipe(gulp.dest('build/css/'));
-  done();
 });
 
 //  Minify html
@@ -38,51 +36,45 @@ gulp.task('minify-html', function(){
 });
 
 //  Gzip html
-gulp.task('compress-html', function(done){
-  gulp.src('build/**/*.html')
+gulp.task('compress-html', function(){
+  return gulp.src('build/**/*.html')
     .pipe(gzip())
     .pipe(gulp.dest('build/'));
-  done();
 });
 
 //  Minify javascripts
-gulp.task('minify-js', function(done){
-  gulp.src('source/js/scroll.js')
+gulp.task('minify-js', function(){
+  return gulp.src('source/js/scroll.js')
     .pipe(uglify().on('error', function(e){
         console.log(e);
      }))
     .pipe(gulp.dest('build/js/'));
-  done();
 });
 
 //  Gzip javascripts
-gulp.task('compress-js', function(done){
-  gulp.src('build/js/scroll.js')
+gulp.task('compress-js', function(){
+  return gulp.src('build/js/scroll.js')
     .pipe(gzip())
     .pipe(gulp.dest('build/js/'));
-  done();
 });
 
 //  Compress images
-gulp.task('compress-images', function(done){
-  gulp.src('source/images/**/*')
+gulp.task('compress-images', function(){
+  return gulp.src('source/images/**/*')
     .pipe(imagemin())
     .pipe(gulp.dest('build/images/'));
-  done();
 });
 
 //  Copy fonts
-gulp.task('copy-fonts', function(done){
-  gulp.src('source/css/fonts/*')
+gulp.task('copy-fonts', function(){
+  return gulp.src('source/css/fonts/*')
     .pipe(gulp.dest('build/css/fonts'));
-  done();
 });
 
 // Copy video
-gulp.task('copy-video', function(done){
-  gulp.src('source/video/*')
+gulp.task('copy-video', function(){
+  return gulp.src('source/video/*')
     .pipe(gulp.dest('build/video'));
-  done();
 });
 
 // Build
